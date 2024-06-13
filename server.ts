906cd605243acefb01bf133568f4dc3dd75b68e1
HariomKumar1012 <hariom15102004@gmail.com>
2024-06-14 05:26:25 +0530

    setup express in the project

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome to E-library");
+import app from "./src/app";
+
+const startServer = () => {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Listning on port: ${port}`);
+    });
+};
+
+startServer();
