5dce6bf1a915539807a4643402832def20a9c86a
HariomKumar1012 <hariom15102004@gmail.com>
2024-06-14 18:58:28 +0530

    add Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "./config/config";
+import globalErrorHandler from "./config/middlewares/globalErrorHandler";
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     res.json({
         message: "Welcome to E library",
     });
 });
+
+//globle Error Handler
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ conf(); //Jab tak ye call nahi hota tab tak dotenv us file me aata nahi hai
 const _config = {
     port: process.env.PORT,
     databaseURL: process.env.MONGO_CONNECTION_STRING,
+    env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config); //Object.freeze make the _config read only
