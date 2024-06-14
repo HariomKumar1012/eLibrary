f00d83d530f14342603432f2410432ea6d956c6c
HariomKumar1012 <hariom15102004@gmail.com>
2024-06-14 20:56:32 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./config/middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.get("/", (req, res) => {
         message: "Welcome to E library",
     });
 });
+
+//user router
+app.use("/api/users", userRouter);
 
 //globle Error Handler
 app.use(globalErrorHandler);
