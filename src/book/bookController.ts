312c793ba5e869ec9efa5f89bdf043d5fc182458
HariomKumar1012 <hariom15102004@gmail.com>
2024-06-15 03:24:51 +0530

    create new book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,11 +1,14 @@
 import path from "node:path";
+import fs from "node:fs";
 import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        console.log("files", req.files);
+        const { title, genre } = req.body;
+        //console.log("files", req.files);
 
         const files = req.files as {
             [fieldname: string]: Express.Multer.File[];
@@ -52,7 +55,26 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
             }
         );
 
-        res.json({});
+        const newBook = await bookModel.create({
+            title,
+            genre,
+            author: "666c8762d2fb6598af48a802",
+            coverImage: uploadResult.secure_url,
+            file: bookFileUploadResult.secure_url,
+        });
+
+        //Delete temp files
+        try {
+            await fs.promises.unlink(filePath);
+            await fs.promises.unlink(bookFilePath);
+        } catch (err) {
+            console.log(err);
+            return next(
+                createHttpError(500, "Error while deleting temporary files")
+            );
+        }
+
+        res.status(201).json({ id: newBook._id });
     } catch (err) {
         console.log(err);
         return next(
